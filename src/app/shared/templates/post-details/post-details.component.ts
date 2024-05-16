7e02cc77d334c0c09953ca8ef6c300732392666e
LuisUcla <caluigi7@gmail.com>
2024-05-16 11:50:38 -0300

    feat add css y method in ts to component post-details

--- src/app/shared/templates/post-details/post-details.component.ts
+++ src/app/shared/templates/post-details/post-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../../../models/post.model';
+import { Platform } from '@ionic/angular';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-post-details',
@@ -13,10 +15,29 @@ export class PostDetailsComponent  implements OnInit {
 
   selectedImage: string = '';
 
-  constructor() { }
+  constructor(public platform: Platform, private utilsService: UtilsService) { }
 
   ngOnInit() {
     this.selectedImage = this.post.images[0];
+  }
+
+  copyPromptToClipboard() {
+    this.utilsService.copyToClipboard(this.post.prompt);
+    this.utilsService.presetToast({
+      message: "Copiado al porta papeles...",
+      icon: 'clipboard-outline',
+      duration: 1000,
+      color: 'primary'
+    })
+  }
+
+  saveImage() {
+    if (this.platform.is('capacitor')) {
+      // compartir
+    } else {
+      // guardar
+      this.utilsService.saveImageInWeb(this.selectedImage)
+    }
   }
 
 }
