bf7a49f6a1d784d3e905c8920b268ff2ce89b9c1
LuisUcla <caluigi7@gmail.com>
2024-05-28 19:33:42 -0300

    feat add post service
    Se anadieron los metodos para obtener los posts y crear uno en la base de datos de mongo db

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Post } from '../models/post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    return this.http.get(environment.baseUrl + environment.posts)
+  }
+
+  createPosts(post: Post) {
+    return this.http.post(environment.baseUrl + environment.posts, post);
+  }
+}
